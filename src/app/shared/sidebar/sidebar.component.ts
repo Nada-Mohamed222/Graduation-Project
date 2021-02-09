c676391646d5291955bfeb66dfc47ac75ee5839e
AbdulrahmanRoshdy <AbdulrahmanRoshdy@outlook.com>
2021-02-09 14:21:37 +0200

    last edit on sidebar and functionality of signup steps

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,3 +1,4 @@
+import { SharingDataService } from './../../modules/freelancer/signup/sharing-data-service/sharing-data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  completedData: any;
 
-  constructor() { }
+  constructor(private _sharingData: SharingDataService) { }
 
   ngOnInit(): void {
+    this.completedData = this._sharingData.isEligible;
   }
 
 }
