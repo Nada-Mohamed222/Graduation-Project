555833eb226f3de038ba0ec59c672351a15355e7
AbdulrahmanRoshdy <AbdulrahmanRoshdy@outlook.com>
2021-02-13 02:13:57 +0200

    Add update to keep data after go forward or backward while signing up

--- src/app/modules/freelancer/signup/hourlyrate/hourlyrate.component.ts
+++ src/app/modules/freelancer/signup/hourlyrate/hourlyrate.component.ts
@@ -10,19 +10,20 @@ import { SharingDataService } from '../sharing-data-service/sharing-data.service
 })
 export class HourlyrateComponent implements OnInit {
 
-  inputMoney: number = 8.00;
-  deductedAmount: number = 2.00;
+  inputMoney: string = "8.00";
+  deductedAmount: string = "2.00";
   formGroup: FormGroup;
   completedData: any;
   constructor(private _formBuilder: FormBuilder, private _sharingData: SharingDataService, private router: Router) { }
 
   ngOnInit(): void {
+    const HourlyRateData: any = this._sharingData.getHourlyRateData();
     this.completedData = this._sharingData.isEligible;
     if (!this._sharingData.isEligible.hourlyRate) {
       this.router.navigateByUrl('/freelancer/languages');
     }
     this.formGroup = this._formBuilder.group({
-      HourlyRate: ['', [Validators.required, Validators.min(5)]]
+      HourlyRate: [HourlyRateData.hourlyRate, [Validators.required, Validators.min(5), Validators.pattern(/^\d{1,3}$/)]]
     })
   }
   //setting HourlyRate from data
@@ -31,12 +32,11 @@ export class HourlyrateComponent implements OnInit {
       hourlyRate: this.formGroup.controls['HourlyRate'].value,
     };
     this._sharingData.setHourlyRateData(HourlyRate);
-    console.log(HourlyRate);
   }
 
   totalMoney(serviceFees: number) {
-    this.inputMoney = serviceFees * 0.8;
-    this.deductedAmount = serviceFees * 0.2;
+    this.inputMoney = (serviceFees * 0.8).toFixed(2);
+    this.deductedAmount = (serviceFees * 0.2).toFixed(2);
   }
 
 }
